e1d22dc96cd61c75acd5c6e1e369abab418209fa
Saqib Nawab <106011021+Saqib-nawab@users.noreply.github.com>
2025-03-10 15:27:27 +0500

    navbar and footer added

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 import Home from "./pages/Home";
 import Rhymes from "./pages/Rhymes";
 import Activities from "./pages/Activities";
@@ -12,16 +14,20 @@ import Privacy from "./pages/Privacy";
 const App: React.FC = () => {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/rhymes" element={<Rhymes />} />
-        <Route path="/activities" element={<Activities />} />
-        <Route path="/blog" element={<Blog />} />
-        <Route path="/subscription" element={<Subscription />} />
-        <Route path="/shop" element={<Shop />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/privacy" element={<Privacy />} />
-      </Routes>
+      <Navbar /> {/* Navbar always visible */}
+      <div className="min-h-screen">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/rhymes" element={<Rhymes />} />
+          <Route path="/activities" element={<Activities />} />
+          <Route path="/blog" element={<Blog />} />
+          <Route path="/subscription" element={<Subscription />} />
+          <Route path="/shop" element={<Shop />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/privacy" element={<Privacy />} />
+        </Routes>
+      </div>
+      <Footer /> {/* Footer always visible */}
     </Router>
   );
 };
