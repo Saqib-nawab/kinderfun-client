af2574ec0c00f74722d7330c7f8c49b37e8b6883
Saqib Nawab <106011021+Saqib-nawab@users.noreply.github.com>
2025-03-10 16:15:26 +0500

    tailwind still not working

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -6,7 +6,7 @@ const Home: React.FC = () => {
       <h1 className="text-4xl font-kinderFun text-primary mb-4">
         Welcome to Kinderfun!
       </h1>
-      <p className="text-lg text-primary">
+      <p className="text-lg text-primary mt-10">
         Explore our collection of engaging rhymes, stories, and activities designed for kids and parents alike.
       </p>
     </div>
