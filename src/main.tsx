2d0e5ca4723db170b5af597bbee40228549571d2
Saqib Nawab <106011021+Saqib-nawab@users.noreply.github.com>
2025-03-11 11:50:28 +0500

    i think tailwind is working now

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,8 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App';
 import './index.css'; // Import your global CSS (including Tailwind CSS)
+import './output.css';
+
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,8 +4,12 @@ import React from 'react';
 const Home: React.FC = () => {
   return (
     <div>
-      <h1>Welcome to Kinderfun!</h1>
-      <p>Engaging visuals with featured videos and interactive elements.</p>
+      <h1>
+        Welcome to Kinderfun!
+      </h1>
+      <p className="">
+        Engaging visuals with featured videos and interactive elements.
+      </p>
     </div>
   );
 };
