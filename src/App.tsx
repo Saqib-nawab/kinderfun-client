94f8565f67ade20698ba18e0dfc0fe0a2b3f80fe
Saqib Nawab <106011021+Saqib-nawab@users.noreply.github.com>
2025-03-11 15:49:12 +0500

    navbar finito for now

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ const App: React.FC = () => {
   return (
     <div className="flex flex-col min-h-screen">
       <Navbar />
-      <main className="flex-grow">
+      <main className="flex-grow mt-16 md:mt-20">
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/rhymes" element={<Rhymes />} />
