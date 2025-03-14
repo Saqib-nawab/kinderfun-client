323b59b421374e500e64089ffaa2ed54479d6130
Saqib Nawab <106011021+Saqib-nawab@users.noreply.github.com>
2025-03-14 16:06:11 +0500

    gamex finito

---
+++ src/components/games/BoxJumpGame.tsx
@@ -0,0 +1,16 @@
+// src/components/BoxJumpGame.tsx
+import React from 'react';
+
+const BoxJumpGame: React.FC = () => {
+    return (
+        <iframe
+            src="https://noowxela.github.io/phaser-examples/games/box-jump/"
+            title="Pixel War Game"
+            width="800"
+            height="600"
+            style={{ border: 'none' }}
+        />
+    );
+};
+
+export default BoxJumpGame;
---
+++ src/components/games/RunPhaserGame.tsx
@@ -0,0 +1,16 @@
+// src/components/RunPhaserGame.tsx
+import React from 'react';
+
+const RunPhaserGame: React.FC = () => {
+  return (
+    <iframe
+      src="https://noowxela.github.io/phaser-examples/games/run/"
+      title="Run Game"
+      width="800"
+      height="600"
+      style={{ border: 'none' }}
+    />
+  );
+};
+
+export default RunPhaserGame;
