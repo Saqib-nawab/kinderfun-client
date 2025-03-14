51cd103dbf00f9fc779f27d9a73c5bd2b281e62a
Saqib Nawab <106011021+Saqib-nawab@users.noreply.github.com>
2025-03-14 12:52:51 +0500

    snake game so far so good

--- src/pages/Activities.tsx
+++ src/pages/Activities.tsx
@@ -1,6 +1,7 @@
 // src/pages/Activities.tsx
 import React from 'react';
 import PuzzlePhaserGame from '../components/PuzzlePhaserGame';
+import SnakePhaserGame from '../components/SnakePhaserGame';
 
 const Activities: React.FC = () => {
   return (
@@ -8,6 +9,7 @@ const Activities: React.FC = () => {
       <h1>Educational Activities</h1>
       <p>Printable worksheets, coloring pages, puzzles, and interactive games.</p>
       <PuzzlePhaserGame />
+      <SnakePhaserGame />
     </div>
   );
 };
